5f6867d0475869b005205e2cb65d804677a4a42d
Anchal Srivastava <anchal@torinvestment.com>
2020-01-20 10:26:35 +0800

    updated missing files

---
+++ CTrader.Interfaces/IStrategySummary.cs
@@ -0,0 +1,14 @@
+﻿namespace CTrader.Interfaces
+{
+    public interface IStrategySummary
+    {
+        string Environment { get; set; }
+        string State { get; set; }
+        decimal WalletBalance { get; set; }
+        decimal AvailableMargin { get; set; }
+        decimal RealizedPnl { get; set; }
+        decimal UnrealizedPnl { get; set; }
+        decimal Leverage { get; set; }
+        decimal UsedMargin { get; set; }
+    }
+}
---
+++ CTrader.WebAPI/WebAPIStartup.cs
@@ -0,0 +1,43 @@
+﻿using Bitmex.NET.Authorization;
+using CTrader.Interfaces;
+using System.Configuration;
+
+namespace CTrader.WebAPI
+{
+    public class WebAPIStartup
+    {
+        static IStrategy _strategy;
+        public static void Start(IStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+        public static void Start()
+        {
+            _strategy.Start();
+        }
+        public static void Stop()
+        {
+            _strategy.Stop();
+        }
+        public static void Rebalance()
+        {
+            _strategy.Rebalance();
+        }
+        public static string GetAPIKey()
+        {
+            return ConfigurationManager.AppSettings["APIKEY"];
+        }
+        public static string GetAPISecret()
+        {
+            return ConfigurationManager.AppSettings["APISECRET"];
+        }
+        public static string GetSignature(int expiresTime)
+        {
+            return (new SignatureProvider()).CreateSignature(WebAPIStartup.GetAPISecret(), $"GET/realtime{expiresTime}");
+        }
+        public static IStrategySummary GetStrategySummary()
+        {
+            return _strategy.GetSummary();
+        }
+    }
+}
